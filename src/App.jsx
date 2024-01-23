44220f7f6460454f495dfb8051d28fa8aa9db8c2
KramYengir <kramyengir@gmail.com>
2024-01-23 17:56:57 +0000

    sidebar mobile complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
+import Sidebar from "./components/Sidebar/Sidebar";
 
 const App = () => {
   return (
     <div>
       <Header />
       <Hero />
+      <Sidebar />
     </div>
   );
 };
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,7 +1,33 @@
 import "./Sidebar.css";
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <section>
+      <aside>
+        <h2>New</h2>
+        <h3>Hydrogen VS Electric Cars</h3>
+        <p>Will hydrogen-fueled cars ever catch up tp EVs?</p>
+        <hr />
+        <h3>The Downside of AI Artistry</h3>
+        <p>
+          The Downsides of AI Artistry What are the possible adverse effects of
+          on-demand AI image generation?
+        </p>
+        <hr />
+        <h3>Is VC Funding Drying Up?</h3>
+        <p>
+          Private funding by VC firms is down 50% YOY. We take a look at what
+          that means.
+        </p>
+      </aside>
+    </section>
+  );
 };
 
 export default Sidebar;
+
+// FOR THIS ASSIGNMENT, I HARDCODED THE INFORMATION ABOVE,
+// HOWEVER, FOR A PROJECT WITH DYNAMIC CONTENT, I WOULD
+// HAVE USED SEPERATE COMPONENTS FOR EACH PIECE OF INFORMATION,
+// WHICH I WOULD HAVE STOED IN AN ARRAY - I.E. THE USUAL
+// REACT WAY
